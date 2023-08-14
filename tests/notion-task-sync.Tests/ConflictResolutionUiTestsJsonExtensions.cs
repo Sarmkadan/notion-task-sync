f9d8e6c0dcb2c5c5f1e4234b07cf01ea9235c14a
Vladyslav Zaiets <rutova2@gmail.com>
2023-08-14 11:18:00 +0300

    feat: add ConflictResolutionUiTestsJson json helpers

---
+++ tests/notion-task-sync.Tests/ConflictResolutionUiTestsJsonExtensions.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace NotionTaskSync.Tests;
+
+public static class ConflictResolutionUiTestsJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="ConflictResolutionUiTests"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation.</param>
+    /// <returns>A JSON string representation of the instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this ConflictResolutionUiTests value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="ConflictResolutionUiTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized instance, or null if the JSON is empty or whitespace.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static ConflictResolutionUiTests? FromJson(string json)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<ConflictResolutionUiTests>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="ConflictResolutionUiTests"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized instance if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    public static bool TryFromJson(string json, out ConflictResolutionUiTests? value)
+    {
+        ArgumentNullException.ThrowIfNull(json);
+
+        value = null;
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return true;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<ConflictResolutionUiTests>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
