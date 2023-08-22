6f40f3a47ff74e83eba79286606d85785f6b971a
Vladyslav Zaiets <rutova2@gmail.com>
2023-08-22 12:30:38 +0300

    feat(models): json serialization for ConflictDiffResultJson

---
+++ Domain/Models/ConflictDiffResultJsonExtensions.cs
@@ -0,0 +1,86 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+namespace NotionTaskSync.Domain.Models;
+
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+/// <summary>
+/// Provides System.Text.Json serialization and deserialization extensions for
+/// <see cref="ConflictDiffResult"/> to support JSON serialization scenarios like
+/// configuration binding, API communication, and storage.
+/// </summary>
+public static class ConflictDiffResultJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        Converters = { new JsonStringEnumConverter(JsonNamingPolicy.CamelCase) }
+    };
+
+    /// <summary>
+    /// Serializes the <see cref="ConflictDiffResult"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The diff result instance to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the diff result.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
+    public static string ToJson(this ConflictDiffResult value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a <see cref="ConflictDiffResult"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized diff result instance, or <see langword="null"/> if the JSON is empty or whitespace.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static ConflictDiffResult? FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        if (string.IsNullOrWhiteSpace(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<ConflictDiffResult>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a <see cref="ConflictDiffResult"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized diff result instance if successful.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    public static bool TryFromJson(string json, out ConflictDiffResult? value)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+
+        try
+        {
+            value = JsonSerializer.Deserialize<ConflictDiffResult>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = null;
+            return false;
+        }
+    }
+}
