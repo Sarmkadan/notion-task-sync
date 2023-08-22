687ae0bb540059ac12a28298318068242129dc18
Vladyslav Zaiets <rutova2@gmail.com>
2023-08-22 06:59:40 +0300

    json serialization for SyncExceptionJson
    found this while looking at something else.

---
+++ Domain/Exceptions/SyncExceptionJsonExtensions.cs
@@ -0,0 +1,77 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace NotionTaskSync.Domain.Exceptions;
+
+public static class SyncExceptionJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        WriteIndented = false,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+    };
+
+    /// <summary>
+    /// Serializes a <see cref="SyncException"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The exception to serialize.</param>
+    /// <param name="indented">Whether to format the JSON with indentation.</param>
+    /// <returns>A JSON string representation of the exception.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    public static string ToJson(this SyncException value, bool indented = false)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions)
+            {
+                WriteIndented = true
+            }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(value, options);
+    }
+
+    /// <summary>
+    /// Deserializes a <see cref="SyncException"/> instance from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <returns>The deserialized exception, or null if the JSON is null or empty.</returns>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static SyncException? FromJson(string json)
+    {
+        if (string.IsNullOrEmpty(json))
+        {
+            return null;
+        }
+
+        return JsonSerializer.Deserialize<SyncException>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a <see cref="SyncException"/> instance from a JSON string.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize.</param>
+    /// <param name="value">Receives the deserialized exception if successful.</param>
+    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
+    public static bool TryFromJson(string json, out SyncException? value)
+    {
+        value = null;
+
+        if (string.IsNullOrEmpty(json))
+        {
+            return true;
+        }
+
+        try
+        {
+            value = JsonSerializer.Deserialize<SyncException>(json, _jsonOptions);
+            return true;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+}
