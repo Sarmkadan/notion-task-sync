914ddc9168cecbd895e0cd2b08a6cda9f6128d7f
Vladyslav Zaiets <rutova2@gmail.com>
2023-09-15 17:18:12 +0300

    Small LoggerFactory helpers

--- Infrastructure/Logging/LoggerFactoryExtensions.cs
+++ Infrastructure/Logging/LoggerFactoryExtensions.cs
@@ -3,16 +3,22 @@ using Microsoft.Extensions.Logging;
 
 namespace NotionTaskSync.Infrastructure.Logging
 {
+    /// <summary>
+    /// Extension methods for <see cref="ILoggerFactory"/> to support file logging operations.
+    /// </summary>
     public static class LoggerFactoryExtensions
     {
         /// <summary>
         /// Ensures the directory for log files exists, creating it if necessary.
         /// </summary>
-        public static void EnsureLogDirectoryExists(this LoggerFactory factory)
+        /// <param name="factory">The logger factory instance.</param>
+        /// <param name="logFilePath">The log file path to ensure directory exists for.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="logFilePath"/> is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="logFilePath"/> is empty or contains only whitespace.</exception>
+        public static void EnsureLogDirectoryExists(this ILoggerFactory factory, string logFilePath)
         {
-            var logFilePath = factory.GetLogFilePath();
-            if (string.IsNullOrEmpty(logFilePath))
-                return;
+            ArgumentNullException.ThrowIfNull(factory);
+            ArgumentException.ThrowIfNullOrWhiteSpace(logFilePath);
 
             var directory = System.IO.Path.GetDirectoryName(logFilePath);
             if (!string.IsNullOrEmpty(directory) && !System.IO.Directory.Exists(directory))
@@ -22,22 +28,158 @@ namespace NotionTaskSync.Infrastructure.Logging
         }
 
         /// <summary>
-        /// Rotates the log file and cleans up old logs with a default retention period of 30 days.
+        /// Rotates the log file if it exceeds the specified maximum size and cleans up old logs.
         /// </summary>
-        public static void RotateAndCleanupLogs(this LoggerFactory factory, int daysToKeep = 30)
+        /// <param name="factory">The logger factory instance.</param>
+        /// <param name="logFilePath">The path to the log file to rotate.</param>
+        /// <param name="maxSizeBytes">Maximum log file size in bytes before rotation (default: 10MB).</param>
+        /// <param name="retentionDays">Number of days to keep old log files (default: 30).</param>
+        /// <exception cref="ArgumentNullException"><paramref name="logFilePath"/> is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="logFilePath"/> is empty or contains only whitespace.</exception>
+        public static void RotateAndCleanupLogs(this ILoggerFactory factory, string logFilePath,
+            long maxSizeBytes = 10485760, int retentionDays = 30)
         {
-            factory.RotateLogFile();
-            factory.CleanupOldLogs(daysToKeep);
+            ArgumentNullException.ThrowIfNull(factory);
+            ArgumentException.ThrowIfNullOrWhiteSpace(logFilePath);
+
+            RotateLogFile(factory, logFilePath, maxSizeBytes);
+            CleanupOldLogs(factory, logFilePath, retentionDays);
         }
 
         /// <summary>
-        /// Validates the log path configuration and throws if invalid.
+        /// Rotates the log file if it exceeds the specified maximum size.
+        /// Creates a timestamped archive of the current log file and starts a new one.
         /// </summary>
-        public static void ValidateLogConfiguration(this LoggerFactory factory)
+        /// <param name="factory">The logger factory instance.</param>
+        /// <param name="logFilePath">The path to the log file to rotate.</param>
+        /// <param name="maxSizeBytes">Maximum log file size in bytes before rotation.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="logFilePath"/> is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="logFilePath"/> is empty or contains only whitespace.</exception>
+        public static void RotateLogFile(this ILoggerFactory factory, string logFilePath, long maxSizeBytes = 10485760)
         {
-            if (!factory.ValidateLogPath())
+            ArgumentNullException.ThrowIfNull(factory);
+            ArgumentException.ThrowIfNullOrWhiteSpace(logFilePath);
+
+            if (!System.IO.File.Exists(logFilePath))
             {
-                throw new InvalidOperationException("Log path configuration is invalid or inaccessible.");
+                return;
+            }
+
+            var fileInfo = new System.IO.FileInfo(logFilePath);
+            if (fileInfo.Length <= maxSizeBytes)
+            {
+                return;
+            }
+
+            var timestamp = DateTime.UtcNow.ToString("yyyyMMdd_HHmmss");
+            var archiveName = System.IO.Path.Combine(
+                System.IO.Path.GetDirectoryName(logFilePath) ?? ".",
+                $"{System.IO.Path.GetFileNameWithoutExtension(logFilePath)}.{timestamp}.log"
+            );
+
+            try
+            {
+                System.IO.File.Move(logFilePath, archiveName);
+            }
+            catch (Exception ex) when (ex is System.IO.IOException or UnauthorizedAccessException)
+            {
+                // Log rotation failed, continue with normal logging
+                // This is not critical, so we don't throw
+            }
+        }
+
+        /// <summary>
+        /// Cleans up old log files based on retention policy.
+        /// </summary>
+        /// <param name="factory">The logger factory instance.</param>
+        /// <param name="logFilePath">The path to the current log file.</param>
+        /// <param name="retentionDays">Number of days to keep old log files.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="logFilePath"/> is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="logFilePath"/> is empty or contains only whitespace.</exception>
+        public static void CleanupOldLogs(this ILoggerFactory factory, string logFilePath, int retentionDays = 30)
+        {
+            ArgumentNullException.ThrowIfNull(factory);
+            ArgumentException.ThrowIfNullOrWhiteSpace(logFilePath);
+
+            var logDir = System.IO.Path.GetDirectoryName(logFilePath);
+            if (string.IsNullOrEmpty(logDir) || !System.IO.Directory.Exists(logDir))
+            {
+                return;
+            }
+
+            var cutoffDate = DateTime.UtcNow.AddDays(-retentionDays);
+            var pattern = $"{System.IO.Path.GetFileNameWithoutExtension(logFilePath)}.*.log";
+
+            try
+            {
+                foreach (var file in System.IO.Directory.GetFiles(logDir, pattern))
+                {
+                    var fileInfo = new System.IO.FileInfo(file);
+                    if (fileInfo.LastWriteTimeUtc < cutoffDate)
+                    {
+                        try
+                        {
+                            System.IO.File.Delete(file);
+                        }
+                        catch (Exception ex) when (ex is System.IO.IOException or UnauthorizedAccessException)
+                        {
+                            // Silently ignore cleanup errors to prevent logging failures
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) when (ex is System.IO.IOException or UnauthorizedAccessException or System.Security.SecurityException)
+            {
+                // Silently ignore directory access errors
+            }
+        }
+
+        /// <summary>
+        /// Validates that the log directory is accessible and writable.
+        /// </summary>
+        /// <param name="factory">The logger factory instance.</param>
+        /// <param name="logFilePath">The log file path to validate.</param>
+        /// <returns>True if the log path is valid and accessible; otherwise, false.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="logFilePath"/> is null.</exception>
+        /// <exception cref="ArgumentException"><paramref name="logFilePath"/> is empty or contains only whitespace.</exception>
+        public static bool ValidateLogPath(this ILoggerFactory factory, string logFilePath)
+        {
+            ArgumentNullException.ThrowIfNull(factory);
+            ArgumentException.ThrowIfNullOrWhiteSpace(logFilePath);
+
+            try
+            {
+                var logDir = System.IO.Path.GetDirectoryName(logFilePath);
+                if (string.IsNullOrEmpty(logDir))
+                {
+                    return false;
+                }
+
+                if (!System.IO.Directory.Exists(logDir))
+                {
+                    return false;
+                }
+
+                // Test write permissions
+                var testFilePath = System.IO.Path.Combine(logDir, $".test_{Guid.NewGuid()}");
+                try
+                {
+                    using (var stream = System.IO.File.Create(testFilePath))
+                    {
+                        stream.WriteByte(0);
+                    }
+
+                    System.IO.File.Delete(testFilePath);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
             }
         }
     }
