d1251aaed9ba68cf8f84c5561a362051378cbb6d
Vladyslav Zaiets <rutova2@gmail.com>
2023-09-19 09:10:09 +0300

    Handy extensions for StringExtensions

--- tests/notion-task-sync.Tests/StringExtensionsTestsExtensions.cs
+++ tests/notion-task-sync.Tests/StringExtensionsTestsExtensions.cs
@@ -4,22 +4,29 @@ namespace NotionTaskSync.Tests
 {
     /// <summary>
     /// Extension methods that make it easier to invoke the existing string‑extension related tests.
+    /// Provides a fluent API for executing test suites on <see cref="StringExtensionsTests"/>.
     /// </summary>
     public static class StringExtensionsTestsExtensions
     {
         /// <summary>
         /// Executes the truncate test.
         /// </summary>
+        /// <param name="test">The test instance to execute the method on.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="test"/> is null.</exception>
         public static void RunTruncateTest(this StringExtensionsTests test)
         {
+            ArgumentNullException.ThrowIfNull(test);
             test.Truncate_StringLongerThanMaxLength_ReturnsTruncatedWithDefaultSuffix();
         }
 
         /// <summary>
         /// Executes both sanitize‑for‑filename tests.
         /// </summary>
+        /// <param name="test">The test instance to execute the methods on.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="test"/> is null.</exception>
         public static void RunSanitizeTests(this StringExtensionsTests test)
         {
+            ArgumentNullException.ThrowIfNull(test);
             test.SanitizeForFilename_EmptyString_ReturnsUntitled();
             test.SanitizeForFilename_StringWithSpaces_ReplacesSpacesWithUnderscores();
         }
@@ -27,16 +34,22 @@ namespace NotionTaskSync.Tests
         /// <summary>
         /// Executes the Pascal‑to‑snake‑case test.
         /// </summary>
+        /// <param name="test">The test instance to execute the method on.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="test"/> is null.</exception>
         public static void RunToSnakeCaseTest(this StringExtensionsTests test)
         {
+            ArgumentNullException.ThrowIfNull(test);
             test.ToSnakeCase_PascalCaseString_ReturnsLowercaseWithUnderscores();
         }
 
         /// <summary>
         /// Executes the slug‑generation test.
         /// </summary>
+        /// <param name="test">The test instance to execute the method on.</param>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="test"/> is null.</exception>
         public static void RunToSlugTest(this StringExtensionsTests test)
         {
+            ArgumentNullException.ThrowIfNull(test);
             test.ToSlug_StringWithPunctuationAndSpaces_ReturnsCleanHyphenatedSlug();
         }
     }
