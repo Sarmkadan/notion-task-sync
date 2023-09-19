05b50430bbe57b86f6b25d7ed42c231edf1233b8
Vladyslav Zaiets <rutova2@gmail.com>
2023-09-19 17:57:05 +0300

    A couple of Task extensions

--- Domain/Models/TaskExtensions.cs
+++ Domain/Models/TaskExtensions.cs
@@ -20,25 +20,32 @@ public static class TaskExtensions
     /// <summary>
     /// Determines whether the task is overdue based on its DueDate and current status.
     /// </summary>
-    /// <param name="task">The task to check</param>
-    /// <returns>True if the task is overdue; otherwise, false</returns>
+    /// <param name="task">The task to check.</param>
+    /// <returns>True if the task is overdue; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="task"/> is null.</exception>
     public static bool IsOverdue(this Task task)
     {
+        ArgumentNullException.ThrowIfNull(task);
+
         if (task.DueDate == null || task.Status == TaskStatus.Done || task.Status == TaskStatus.Archived)
             return false;
 
-        return task.DueDate.Value < DateTime.UtcNow.Date;
+        return task.DueDate.Value.Date < DateTime.UtcNow.Date;
     }
 
     /// <summary>
     /// Determines whether the task is due today.
     /// </summary>
-    /// <param name="task">The task to check</param>
-    /// <returns>True if the task is due today; otherwise, false</returns>
+    /// <param name="task">The task to check.</param>
+    /// <returns>True if the task is due today; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="task"/> is null.</exception>
     public static bool IsDueToday(this Task task)
     {
+        ArgumentNullException.ThrowIfNull(task);
+
         if (task.DueDate == null)
             return false;
+
 
         var today = DateTime.UtcNow.Date;
         return task.DueDate.Value.Date == today;
@@ -47,50 +54,65 @@ public static class TaskExtensions
     /// <summary>
     /// Determines whether the task is high priority (priority >= 80).
     /// </summary>
-    /// <param name="task">The task to check</param>
-    /// <returns>True if the task is high priority; otherwise, false</returns>
+    /// <param name="task">The task to check.</param>
+    /// <returns>True if the task is high priority; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="task"/> is null.</exception>
     public static bool IsHighPriority(this Task task)
     {
+        ArgumentNullException.ThrowIfNull(task);
+
         return task.Priority >= 80;
     }
 
     /// <summary>
     /// Determines whether the task is blocked or has a blocked status.
     /// </summary>
-    /// <param name="task">The task to check</param>
-    /// <returns>True if the task is blocked; otherwise, false</returns>
+    /// <param name="task">The task to check.</param>
+    /// <returns>True if the task is blocked; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="task"/> is null.</exception>
     public static bool IsBlocked(this Task task)
     {
+        ArgumentNullException.ThrowIfNull(task);
+
         return task.Status == TaskStatus.Blocked;
     }
 
     /// <summary>
     /// Gets the age of the task in days since creation.
     /// </summary>
-    /// <param name="task">The task to calculate age for</param>
-    /// <returns>Number of days since task was created</returns>
+    /// <param name="task">The task to calculate age for.</param>
+    /// <returns>Number of days since task was created.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="task"/> is null.</exception>
     public static int GetAgeInDays(this Task task)
     {
+        ArgumentNullException.ThrowIfNull(task);
+
         return (int)(DateTime.UtcNow.Date - task.CreatedAt.Date).TotalDays;
     }
 
     /// <summary>
     /// Determines whether the task is recent (created within last 7 days).
     /// </summary>
-    /// <param name="task">The task to check</param>
-    /// <returns>True if the task is recent; otherwise, false</returns>
+    /// <param name="task">The task to check.</param>
+    /// <returns>True if the task is recent; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="task"/> is null.</exception>
     public static bool IsRecent(this Task task)
     {
+        ArgumentNullException.ThrowIfNull(task);
+
         return (DateTime.UtcNow.Date - task.CreatedAt.Date).TotalDays <= 7;
     }
 
     /// <summary>
     /// Gets the task's priority level as a descriptive string.
     /// </summary>
-    /// <param name="task">The task to get priority for</param>
-    /// <returns>Priority level description (Critical, High, Medium, Low)</returns>
+    /// <param name="task">The task to get priority for.</param>
+    /// <returns>Priority level description (Critical, High, Medium, Low).</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="task"/> is null.</exception>
     public static string GetPriorityLevel(this Task task)
     {
+        ArgumentNullException.ThrowIfNull(task);
+
         return task.Priority switch
         {
             >= 90 => "Critical",
@@ -103,20 +125,26 @@ public static class TaskExtensions
     /// <summary>
     /// Determines whether the task has any tags.
     /// </summary>
-    /// <param name="task">The task to check</param>
-    /// <returns>True if the task has tags; otherwise, false</returns>
+    /// <param name="task">The task to check.</param>
+    /// <returns>True if the task has tags; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="task"/> is null.</exception>
     public static bool HasTags(this Task task)
     {
+        ArgumentNullException.ThrowIfNull(task);
+
         return !string.IsNullOrWhiteSpace(task.Tags);
     }
 
     /// <summary>
     /// Gets the tags as a collection of strings split by comma.
     /// </summary>
-    /// <param name="task">The task to get tags from</param>
-    /// <returns>Collection of tag strings</returns>
+    /// <param name="task">The task to get tags from.</param>
+    /// <returns>Collection of tag strings.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="task"/> is null.</exception>
     public static IEnumerable<string> GetTagList(this Task task)
     {
+        ArgumentNullException.ThrowIfNull(task);
+
         if (string.IsNullOrWhiteSpace(task.Tags))
             return Array.Empty<string>();
 
@@ -128,12 +156,19 @@ public static class TaskExtensions
     /// <summary>
     /// Determines whether the task matches the specified tag.
     /// </summary>
-    /// <param name="task">The task to check</param>
-    /// <param name="tag">The tag to match</param>
-    /// <returns>True if the task has the specified tag; otherwise, false</returns>
+    /// <param name="task">The task to check.</param>
+    /// <param name="tag">The tag to match.</param>
+    /// <returns>True if the task has the specified tag; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if <paramref name="task"/> is null.
+    /// Thrown if <paramref name="tag"/> is null or whitespace.
+    /// </exception>
     public static bool HasTag(this Task task, string tag)
     {
-        if (string.IsNullOrWhiteSpace(task.Tags) || string.IsNullOrWhiteSpace(tag))
+        ArgumentNullException.ThrowIfNull(task);
+        ArgumentException.ThrowIfNullOrWhiteSpace(tag);
+
+        if (string.IsNullOrWhiteSpace(task.Tags))
             return false;
 
         var tags = task.GetTagList();
