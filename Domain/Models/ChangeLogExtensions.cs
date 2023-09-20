b6537224f71118adabfa8a78edf2e44230a7c226
Vladyslav Zaiets <rutova2@gmail.com>
2023-09-20 18:48:29 +0300

    small ChangeLog helpers

--- Domain/Models/ChangeLogExtensions.cs
+++ Domain/Models/ChangeLogExtensions.cs
@@ -3,7 +3,7 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =============================================================================
+// =====================================================================
 
 namespace NotionTaskSync.Domain.Models;
 
@@ -11,7 +11,7 @@ using System;
 using System.Text;
 
 /// <summary>
-/// Extension methods for ChangeLog providing additional functionality for audit trail operations.
+/// Extension methods for <see cref="ChangeLog"/> providing additional functionality for audit trail operations.
 /// </summary>
 public static class ChangeLogExtensions
 {
@@ -19,40 +19,40 @@ public static class ChangeLogExtensions
     /// Creates a detailed audit trail entry string for logging and reporting purposes.
     /// Includes all relevant change information in a human-readable format.
     /// </summary>
-    /// <param name="changeLog">The change log entry to format</param>
-    /// <param name="includeValues">Whether to include old/new value comparisons</param>
-    /// <returns>Formatted audit string</returns>
+    /// <param name="changeLog">The change log entry to format. Cannot be <see langword="null"/>.</param>
+    /// <param name="includeValues">Whether to include old/new value comparisons.</param>
+    /// <returns>Formatted audit string.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="changeLog"/> is <see langword="null"/>.</exception>
     public static string ToAuditString(this ChangeLog changeLog, bool includeValues = true)
     {
-        if (changeLog == null)
-            throw new ArgumentNullException(nameof(changeLog));
+        ArgumentNullException.ThrowIfNull(changeLog);
 
         var sb = new StringBuilder();
         sb.AppendLine($"Change Log Entry: {changeLog.Id}");
-        sb.AppendLine($"  Task ID: {changeLog.TaskId}");
-        sb.AppendLine($"  Type: {changeLog.ChangeType}");
-        sb.AppendLine($"  Source: {changeLog.Source}");
-        sb.AppendLine($"  Timestamp: {changeLog.Timestamp:yyyy-MM-dd HH:mm:ss UTC}");
+        sb.AppendLine($" Task ID: {changeLog.TaskId}");
+        sb.AppendLine($" Type: {changeLog.ChangeType}");
+        sb.AppendLine($" Source: {changeLog.Source}");
+        sb.AppendLine($" Timestamp: {changeLog.Timestamp:yyyy-MM-dd HH:mm:ss UTC}");
 
         if (!string.IsNullOrEmpty(changeLog.UserEmail))
-            sb.AppendLine($"  User: {changeLog.UserEmail}");
+            sb.AppendLine($" User: {changeLog.UserEmail}");
 
         if (!string.IsNullOrEmpty(changeLog.PropertyName))
-            sb.AppendLine($"  Property: {changeLog.PropertyName}");
+            sb.AppendLine($" Property: {changeLog.PropertyName}");
 
         if (includeValues && !string.IsNullOrEmpty(changeLog.OldValue) && !string.IsNullOrEmpty(changeLog.NewValue))
         {
-            sb.AppendLine($"  Old Value: {changeLog.OldValue}");
-            sb.AppendLine($"  New Value: {changeLog.NewValue}");
+            sb.AppendLine($" Old Value: {changeLog.OldValue}");
+            sb.AppendLine($" New Value: {changeLog.NewValue}");
         }
 
         if (!string.IsNullOrEmpty(changeLog.Description))
-            sb.AppendLine($"  Description: {changeLog.Description}");
+            sb.AppendLine($" Description: {changeLog.Description}");
 
         if (changeLog.IsConflict)
         {
-            sb.AppendLine("  CONFLICT STATUS:");
-            sb.AppendLine($"    Resolved: {changeLog.ConflictResolutionStrategy ?? "Unknown"}");
+            sb.AppendLine(" CONFLICT STATUS:");
+            sb.AppendLine($" Resolved: {changeLog.ConflictResolutionStrategy ?? "Unknown"}");
         }
 
         return sb.ToString().Trim();
@@ -62,12 +62,12 @@ public static class ChangeLogExtensions
     /// Determines if this change represents a property modification.
     /// Useful for filtering change logs to only property changes.
     /// </summary>
-    /// <param name="changeLog">The change log entry to check</param>
-    /// <returns>True if this is a property change, false otherwise</returns>
+    /// <param name="changeLog">The change log entry to check. Cannot be <see langword="null"/>.</param>
+    /// <returns>True if this is a property change, false otherwise.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="changeLog"/> is <see langword="null"/>.</exception>
     public static bool IsPropertyChange(this ChangeLog changeLog)
     {
-        if (changeLog == null)
-            throw new ArgumentNullException(nameof(changeLog));
+        ArgumentNullException.ThrowIfNull(changeLog);
 
         return !string.IsNullOrEmpty(changeLog.PropertyName);
     }
@@ -75,35 +75,36 @@ public static class ChangeLogExtensions
     /// <summary>
     /// Creates a simplified change description suitable for UI display or notifications.
     /// </summary>
-    /// <param name="changeLog">The change log entry</param>
-    /// <param name="maxLength">Maximum length of the resulting string</param>
-    /// <returns>Simplified change description</returns>
+    /// <param name="changeLog">The change log entry. Cannot be <see langword="null"/>.</param>
+    /// <param name="maxLength">Maximum length of the resulting string. Must be positive.</param>
+    /// <returns>Simplified change description.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="changeLog"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="maxLength"/> is not positive.</exception>
     public static string ToDisplayString(this ChangeLog changeLog, int maxLength = 100)
     {
-        if (changeLog == null)
-            throw new ArgumentNullException(nameof(changeLog));
+        ArgumentNullException.ThrowIfNull(changeLog);
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(maxLength, 0);
 
         var summary = changeLog.GetSummary();
 
         if (changeLog.IsConflict)
             summary = "⚠️ " + summary + " (Conflict: " + (changeLog.ConflictResolutionStrategy ?? "Pending") + ")";
 
-        if (summary.Length > maxLength)
-            summary = summary.Substring(0, maxLength - 3) + "...";
-
-        return summary;
+        return summary.Length > maxLength
+            ? summary[..(maxLength - 3)] + "..."
+            : summary;
     }
 
     /// <summary>
     /// Determines if this change was made by a user (has UserEmail populated).
     /// Useful for distinguishing system changes from user-initiated changes.
     /// </summary>
-    /// <param name="changeLog">The change log entry to check</param>
-    /// <returns>True if this change was made by a user</returns>
+    /// <param name="changeLog">The change log entry to check. Cannot be <see langword="null"/>.</param>
+    /// <returns>True if this change was made by a user.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="changeLog"/> is <see langword="null"/>.</exception>
     public static bool IsUserInitiated(this ChangeLog changeLog)
     {
-        if (changeLog == null)
-            throw new ArgumentNullException(nameof(changeLog));
+        ArgumentNullException.ThrowIfNull(changeLog);
 
         return !string.IsNullOrEmpty(changeLog.UserEmail);
     }
