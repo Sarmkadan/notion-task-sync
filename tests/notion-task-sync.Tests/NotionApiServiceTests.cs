ba6c62f7f51adef1369f3d38f3881c0f9ebd0e35
Vladyslav Zaiets <rutova2@gmail.com>
2023-09-29 17:52:24 +0300

    Reshuffle

--- tests/notion-task-sync.Tests/NotionApiServiceTests.cs
+++ tests/notion-task-sync.Tests/NotionApiServiceTests.cs
@@ -17,11 +17,25 @@ using System.Threading.Tasks;
 using Task = System.Threading.Tasks.Task;
 using DomainTask = NotionTaskSync.Domain.Models.Task;
 
+/// <summary>
+/// Contains unit tests for the <see cref="NotionApiService"/> class.
+/// Tests the interaction with the Notion API through HTTP requests, including
+/// page fetching, creation, updating, and error handling scenarios.
+/// </summary>
 public class NotionApiServiceTests
 {
-    private readonly Mock<HttpMessageHandler> _mockHttpHandler;
-    private readonly HttpClient _httpClient;
-    private readonly NotionApiService _apiService;
+    /// <summary>
+/// Mock HTTP message handler for testing HTTP requests to the Notion API.
+/// </summary>
+private readonly Mock<HttpMessageHandler> _mockHttpHandler;
+    /// <summary>
+/// HTTP client used for making requests to the Notion API.
+/// </summary>
+private readonly HttpClient _httpClient;
+    /// <summary>
+/// Instance of the NotionApiService being tested.
+/// </summary>
+private readonly NotionApiService _apiService;
 
     public NotionApiServiceTests()
     {
@@ -30,6 +44,14 @@ public class NotionApiServiceTests
         _apiService = new NotionApiService("notionso_test_api_key_" + "".PadRight(20, 'a'), _httpClient);
     }
 
+    /// <summary>
+    /// Tests that FetchPagesAsync successfully retrieves pages from Notion database when given a valid database ID.
+    /// Verifies that the method returns a non-null list of NotionPage objects.
+    /// </summary>
+    /// <summary>
+    /// Tests that FetchPagesAsync successfully retrieves pages from Notion database when given a valid database ID.
+    /// Verifies that the method returns a non-null list of NotionPage objects.
+    /// </summary>
     [Fact]
     public async Task FetchPagesAsync_WithValidDatabaseId_ReturnsPages()
     {
@@ -57,6 +79,10 @@ public class NotionApiServiceTests
         result.Should().BeOfType<List<NotionPage>>();
     }
 
+    /// <summary>
+    /// Tests that FetchPagesAsync throws a ValidationException when provided with an empty database ID.
+    /// Verifies proper input validation for empty strings.
+    /// </summary>
     [Fact]
     public async Task FetchPagesAsync_WithEmptyDatabaseId_ThrowsValidationException()
     {
@@ -64,6 +90,10 @@ public class NotionApiServiceTests
         await Assert.ThrowsAsync<ValidationException>(() => _apiService.FetchPagesAsync(string.Empty));
     }
 
+    /// <summary>
+    /// Tests that FetchPagesAsync throws a ValidationException when provided with a null database ID.
+    /// Verifies proper input validation for null values.
+    /// </summary>
     [Fact]
     public async Task FetchPagesAsync_WithNullDatabaseId_ThrowsValidationException()
     {
@@ -71,6 +101,10 @@ public class NotionApiServiceTests
         await Assert.ThrowsAsync<ValidationException>(() => _apiService.FetchPagesAsync(null!));
     }
 
+    /// <summary>
+    /// Tests that FetchPagesAsync correctly handles pagination with default page size.
+    /// Verifies that the API service makes exactly one HTTP request when fetching pages.
+    /// </summary>
     [Fact]
     public async Task FetchPagesAsync_WithDefaultPageSize_UsesPaginationCorrectly()
     {
@@ -102,6 +136,10 @@ public class NotionApiServiceTests
             ItExpr.IsAny<CancellationToken>());
     }
 
+    /// <summary>
+    /// Tests that FetchPagesAsync throws a NotionApiException when the HTTP request fails.
+    /// Verifies proper error handling for network and API errors.
+    /// </summary>
     [Fact]
     public async Task FetchPagesAsync_WhenHttpRequestFails_ThrowsNotionApiException()
     {
@@ -120,6 +158,10 @@ public class NotionApiServiceTests
         await Assert.ThrowsAsync<NotionApiException>(() => _apiService.FetchPagesAsync(databaseId));
     }
 
+    /// <summary>
+    /// Tests that FetchPagesSinceAsync successfully retrieves and filters pages based on last edited time.
+    /// Verifies that the method returns a non-null list of NotionPage objects.
+    /// </summary>
     [Fact]
     public async Task FetchPagesSinceAsync_WithValidInputs_ReturnsFilteredPages()
     {
@@ -148,6 +190,10 @@ public class NotionApiServiceTests
         result.Should().BeOfType<List<NotionPage>>();
     }
 
+    /// <summary>
+    /// Tests that FetchPagesSinceAsync throws a ValidationException when provided with an empty database ID.
+    /// Verifies proper input validation for empty strings in the filtered fetch operation.
+    /// </summary>
     [Fact]
     public async Task FetchPagesSinceAsync_WithEmptyDatabaseId_ThrowsValidationException()
     {
@@ -159,6 +205,10 @@ public class NotionApiServiceTests
             _apiService.FetchPagesSinceAsync(string.Empty, since));
     }
 
+    /// <summary>
+    /// Tests that FetchPagesSinceAsync correctly filters pages by last edited time.
+    /// Verifies that pagination is used correctly when fetching filtered pages.
+    /// </summary>
     [Fact]
     public async Task FetchPagesSinceAsync_FiltersByLastEditedTime()
     {
@@ -190,6 +240,10 @@ public class NotionApiServiceTests
             ItExpr.IsAny<CancellationToken>());
     }
 
+    /// <summary>
+    /// Tests that FetchPagesSinceAsync returns an empty list when provided with a future timestamp.
+    /// Verifies that the filtering logic correctly handles timestamps in the future.
+    /// </summary>
     [Fact]
     public async Task FetchPagesSinceAsync_WithFutureTimestamp_ReturnsEmptyList()
     {
@@ -218,6 +272,10 @@ public class NotionApiServiceTests
         result.Should().BeOfType<List<NotionPage>>();
     }
 
+    /// <summary>
+    /// Tests that the NotionApiService constructor properly sets the Authorization header when provided with a valid API key.
+    /// Verifies that the Bearer token is correctly configured in the HTTP client.
+    /// </summary>
     [Fact]
     public void Constructor_WithValidApiKey_SetsAuthHeader()
     {
@@ -232,6 +290,10 @@ public class NotionApiServiceTests
         _httpClient.DefaultRequestHeaders.Authorization.Should().NotBeNull();
     }
 
+    /// <summary>
+    /// Tests that the NotionApiService constructor creates a service instance even when provided with a null API key.
+    /// Verifies that the service can be instantiated without authentication for testing purposes.
+    /// </summary>
     [Fact]
     public void Constructor_WithNullApiKey_CreatesServiceWithoutAuth()
     {
@@ -245,6 +307,10 @@ public class NotionApiServiceTests
         service.Should().NotBeNull();
     }
 
+    /// <summary>
+    /// Tests that the NotionApiService constructor creates a default HttpClient when none is provided.
+    /// Verifies that the service can be instantiated with minimal dependencies.
+    /// </summary>
     [Fact]
     public void Constructor_WithoutHttpClient_CreatesDefaultHttpClient()
     {
@@ -258,6 +324,10 @@ public class NotionApiServiceTests
         service.Should().NotBeNull();
     }
 
+    /// <summary>
+    /// Tests that FetchPagesAsync correctly handles empty results from the Notion API.
+    /// Verifies that an empty list is returned when no pages are available.
+    /// </summary>
     [Fact]
     public async Task FetchPagesAsync_HandlesEmptyResults()
     {
@@ -284,6 +354,10 @@ public class NotionApiServiceTests
         result.Should().BeEmpty();
     }
 
+    /// <summary>
+    /// Tests that CreatePageAsync successfully creates a new page in Notion database.
+    /// Verifies that the method returns a NotionPage with the correct PageId.
+    /// </summary>
     [Fact]
     public async Task CreatePageAsync_WithValidTask_CreatesPage()
     {
@@ -318,6 +392,10 @@ public class NotionApiServiceTests
         result.PageId.Should().Be("page123");
     }
 
+    /// <summary>
+    /// Tests that UpdatePageAsync successfully updates an existing page in Notion.
+    /// Verifies that the method returns a NotionPage with the correct PageId.
+    /// </summary>
     [Fact]
     public async Task UpdatePageAsync_WithValidPageAndTask_UpdatesPage()
     {
@@ -352,6 +430,10 @@ public class NotionApiServiceTests
         result.PageId.Should().Be(pageId);
     }
 
+    /// <summary>
+    /// Tests that CreatePageAsync throws a ValidationException when provided with an empty database ID.
+    /// Verifies proper input validation for the page creation operation.
+    /// </summary>
     [Fact]
     public async Task CreatePageAsync_WithEmptyDatabaseId_ThrowsValidationException()
     {
@@ -369,6 +451,10 @@ public class NotionApiServiceTests
             _apiService.CreatePageAsync(string.Empty, task));
     }
 
+    /// <summary>
+    /// Tests that UpdatePageAsync throws a ValidationException when provided with an empty page ID.
+    /// Verifies proper input validation for the page update operation.
+    /// </summary>
     [Fact]
     public async Task UpdatePageAsync_WithEmptyPageId_ThrowsValidationException()
     {
@@ -386,6 +472,10 @@ public class NotionApiServiceTests
             _apiService.UpdatePageAsync(string.Empty, task));
     }
 
+    /// <summary>
+    /// Tests that FetchPagesAsync includes the Notion API version header in requests.
+    /// Verifies that the service sets the required 'Notion-Version' header for API compatibility.
+    /// </summary>
     [Fact]
     public async Task FetchPagesAsync_IncludesNotionApiVersionHeader()
     {
@@ -412,6 +502,10 @@ public class NotionApiServiceTests
         _httpClient.DefaultRequestHeaders.Should().Contain(h => h.Key == "Notion-Version");
     }
 
+    /// <summary>
+    /// Tests that FetchPagesAsync includes the Bearer token in the Authorization header.
+    /// Verifies that the API key is correctly formatted as a Bearer token in HTTP requests.
+    /// </summary>
     [Fact]
     public async Task FetchPagesAsync_IncludesBearerTokenInAuthHeader()
     {
