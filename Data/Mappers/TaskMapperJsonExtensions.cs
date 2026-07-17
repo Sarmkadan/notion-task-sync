52dbb14776fe96c2866d3de8cf8173d437187a25
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    polish TaskMapperJsonExtensions.

--- Data/Mappers/TaskMapperJsonExtensions.cs
+++ Data/Mappers/TaskMapperJsonExtensions.cs
@@ -2,7 +2,7 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =====================================================================
+// ===================================================================
 
 namespace NotionTaskSync.Data.Mappers;
 
@@ -12,7 +12,7 @@ using System.Text.Json.Serialization;
 using NotionTaskSync.Domain.Models;
 
 /// <summary>
-/// Provides System.Text.Json serialization and deserialization extensions for Task entities.
+/// Provides System.Text.Json serialization and deserialization extensions for <see cref="Task"/> entities.
 /// </summary>
 public static class TaskMapperJsonExtensions
 {
@@ -25,12 +25,12 @@ public static class TaskMapperJsonExtensions
     };
 
     /// <summary>
-    /// Serializes a Task instance to a JSON string.
+    /// Serializes a <see cref="Task"/> instance to a JSON string.
     /// </summary>
-    /// <param name="value">The Task instance to serialize.</param>
+    /// <param name="value">The <see cref="Task"/> instance to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
-    /// <returns>A JSON string representation of the Task.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
+    /// <returns>A JSON string representation of the <see cref="Task"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this Task value, bool indented = false)
     {
         ArgumentNullException.ThrowIfNull(value);
@@ -43,11 +43,11 @@ public static class TaskMapperJsonExtensions
     }
 
     /// <summary>
-    /// Deserializes a JSON string to a Task instance.
+    /// Deserializes a JSON string to a <see cref="Task"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>A Task instance, or null if the JSON is empty or whitespace.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    /// <returns>A <see cref="Task"/> instance, or <see langword="null"/> if the JSON is empty or whitespace.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/>, empty, or whitespace.</exception>
     /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
     public static Task? FromJson(string json)
     {
@@ -62,12 +62,12 @@ public static class TaskMapperJsonExtensions
     }
 
     /// <summary>
-    /// Attempts to deserialize a JSON string to a Task instance.
+    /// Attempts to deserialize a JSON string to a <see cref="Task"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">Receives the deserialized Task instance if successful.</param>
-    /// <returns>True if deserialization succeeded; otherwise, false.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    /// <param name="value">Receives the deserialized <see cref="Task"/> instance if successful.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/> or empty.</exception>
     public static bool TryFromJson(string json, out Task? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
