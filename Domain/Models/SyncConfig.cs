72650229cae0585b5c1228a48c05e2fec046be1e
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-24 08:30:01 +0300

    SycnConfig.cs

--- Domain/Models/SyncConfig.cs
+++ Domain/Models/SyncConfig.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
 
 /// <summary>
 /// Configuration for sync settings between local tasks and Notion.
